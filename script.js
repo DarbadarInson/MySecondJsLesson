0d1f96f9147339daa5939d9c0dc1b68950d97716
Ozodbek <devanabuildmuber@gmail.com>
2024-01-06 21:30:54 +0500

    this is my JavaScript 2 Lesson

---
+++ script.js
@@ -0,0 +1,82 @@
+var x = 5;
+var y = 6;
+var z = x + y;
+console.log(x,y,z)
+
+const price1 = 25;
+const price2 = 13;
+let total = price1 + price2;
+console.log(total)
+
+let a = 13
+let b = 13
+let d = a + b
+console.log(d)
+
+let q = 5 + 3 + 2
+console.log(q)
+
+let userName = "Devana" + "." + "Developer"
+console.log(userName)
+
+let math = 8 + 5 + "=" + "13"
+
+
+var o;
+let h;
+o = 13;
+h = 13;
+let s = o + h;
+console.log(math, "|", s)
+
+/*
+const PI = 3.1415
+PI = 3.15
+console.log(PI)
+
+#This is Error
+*/
+
+
+const cars = ["Saab", "Volvo", "BMW"];
+cars[0] = "Toyota";
+cars.push("Audi");
+console.log(cars)
+
+
+const car = {type:"Fiat", model:"500", color:"white"};
+car.color = "red";
+car.owner = "Johnson";
+console.log(car)
+
+let m = 13
+let n = 4
+let v1 = m + n 
+let v2 = m * n
+let v3 = m - n
+let v4 = m / n
+
+console.log("+, " + v1, "*, " + v2, "-, " + v3, "/, " + v4)
+
+let a1 = 13
+let a2 = (8 + 5) * 13
+console.log(a1 + "=" + a2)
+
+let text1 = "What a very ";
+text1 += "nice day"; 
+console.log(text1)
+
+let text2 = "20";
+let text3 = "5";
+let result = text2 > text3;
+let result2 = text2 < text3;
+console.log(result + " | " + result2)
+
+
+
+
+
+
+
+
+
